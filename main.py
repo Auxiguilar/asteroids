4fa62e80c8f98da41f26be478de36bfd61014814
Auxiguilar <filip.myras@gmail.com>
2025-09-09 18:14:46 +0200

    added a player spaceship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -13,11 +14,16 @@ Screen height: {SCREEN_HEIGHT}''')
     clock = pygame.time.Clock()
     dt = 0
 
-    while True:
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    running = True
+
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
