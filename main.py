323444bebb3fb5f61cb34d12b9ccedc590ed20b0
Auxiguilar <filip.myras@gmail.com>
2025-09-09 20:53:58 +0200

    thanks for nothing, boots

--- main.py
+++ main.py
@@ -10,22 +10,25 @@ Screen height: {SCREEN_HEIGHT}''')
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
-
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for u in updatable:
+            u.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
